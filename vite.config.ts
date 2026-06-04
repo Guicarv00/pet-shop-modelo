6a37e5a6782639593d0d216df9953c862801e920
Guilherme Carvalho <andguijuba@gmail.com>
2026-06-04 20:21:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+     base: "/pet-shop-modelo/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
